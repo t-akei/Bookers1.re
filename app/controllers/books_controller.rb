8553709a6ddfe9664250300a7fdd3b31b18b10fb
t-akei <145541870+t-akei@users.noreply.github.com>
2023-11-22 10:18:34 +0000

    [課題2.re]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,15 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def create
+    book = Book.new(book_params)
+    if book.save
+      redirect_to book_path(book)
+    else
+      render :index
+    end
   end
 
   def index
@@ -19,4 +26,12 @@ class BooksController < ApplicationController
 
   def destroy
   end
+
+
+private
+
+def book_params
+  params.require(:book).permit(:title, :body)
+end
+
 end
