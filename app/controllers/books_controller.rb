de8e9ecac1eb15b91fb014abd0584d94cc328772
t-akei <145541870+t-akei@users.noreply.github.com>
2023-11-22 14:52:47 +0000

    [課題2.re]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,7 +27,7 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    if book.save(book_params)
+    if book.update(book_params)
       redirect_to book_path(book)
     else
       render :edit
